e7d5d35d8ba7994de6299af451c467a009353fef
Carlo <carlomjavier@gmail.com>
2019-07-02 21:25:57 +0000

    set the initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: {name: 'Bob'},
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          username: 'Anonynomous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+  }
+  render() {  
     return (
       <div>
-        <NavBar/>
-        <MessageList/>
-        <ChatBar/>
+        <NavBar />
+        <MessageList />
+        <ChatBar />
       </div>
     );
   }
 }
+
 export default App;
